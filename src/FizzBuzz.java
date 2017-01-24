40c82534d646b738787ae3a50f68db7bd56db54e
pacroy <pacroy@gmail.com>
2017-01-24 22:05:45 +0700

    Refactor your code by merge down "if" conditions
    Now, we use modulus operator (%) to determine if the number is a
    multiple of 3 so we can now have only one "if" for every Fizz numbers.
    Run the test and see the green bar to make sure you didn't break
    anything during refactoring.

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -2,9 +2,8 @@
 public class FizzBuzz {
 
 	public String say(int i) {
-		if (i == 3 * 2) return "Fizz";
 		if (i == 5) return "Buzz";
-		if (i == 3) return "Fizz";
+		if (i % 3 == 0) return "Fizz";
 		return String.valueOf(i);
 	}
 
