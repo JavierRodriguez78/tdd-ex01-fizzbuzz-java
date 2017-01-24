2b844202ff7abb42ef7a12299da948c48460d7fb
pacroy <pacroy@gmail.com>
2017-01-24 15:22:59 +0700

    Create a factory method "getNewFizzBuzz" in the test class
    Why don't use "setup" method? The answer is that the setup and teardown
    method make readability of the test codes bad with following reasons:
    
    1) setup and teardown are implicitly called before and after each test
    method. Their names NEVER be meaningful to tell what they gonna do.
    
    2) Imagine you have a lot of test methods. When someone want to read
    your code, he need to scroll to find the setup method first, then scroll
    to your test method and then scroll to teardown method before he
    understand the whole things.
    
    Create our own method is way better as we can name it meaningful enough
    so people can understand at a glance to the test method.

--- src/FizzBuzzTest.java
+++ src/FizzBuzzTest.java
@@ -6,15 +6,19 @@ public class FizzBuzzTest {
 
 	@Test
 	public void should_return_number_when_not_multiple_of_any() {
-		FizzBuzz fb = new FizzBuzz();
+		FizzBuzz fb = getNewFizzBuzz();
 		assertEquals("1", fb.say(1));
 		assertEquals("2", fb.say(2));
 	}
 
 	@Test
 	public void should_return_fizz_when_multiples_of_three() {
-		FizzBuzz fb = new FizzBuzz();
+		FizzBuzz fb = getNewFizzBuzz();
 		assertEquals("Fizz", fb.say(3));
+	}
+
+	private FizzBuzz getNewFizzBuzz() {
+		return new FizzBuzz();
 	}
 
 }
