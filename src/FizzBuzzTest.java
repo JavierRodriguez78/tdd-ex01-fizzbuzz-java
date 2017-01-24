b0fb5dd2a6d897da0340ac2b36f6aaa53f845e4f
pacroy <pacroy@gmail.com>
2017-01-24 15:11:17 +0700

    Refactor test methods by giving them meaningful names
    We need to split the test case with input = 3 into a new test method
    because it is a different scenario than ones with input = 1 and 2.

--- src/FizzBuzzTest.java
+++ src/FizzBuzzTest.java
@@ -5,10 +5,15 @@ import org.junit.Test;
 public class FizzBuzzTest {
 
 	@Test
-	public void test() {
+	public void should_return_number_when_not_multiple_of_any() {
 		FizzBuzz fb = new FizzBuzz();
 		assertEquals("1", fb.say(1));
 		assertEquals("2", fb.say(2));
+	}
+
+	@Test
+	public void should_return_fizz_when_multiples_of_three() {
+		FizzBuzz fb = new FizzBuzz();
 		assertEquals("Fizz", fb.say(3));
 	}
 
