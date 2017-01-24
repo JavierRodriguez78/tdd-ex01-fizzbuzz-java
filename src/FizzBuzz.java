d4ba883df65167cf5e0425000aedb85750db49d0
pacroy <pacroy@gmail.com>
2017-01-24 22:15:16 +0700

    Make the test case input=10, output=Buzz passed
    Now, let's take a bit bigger step here as you know this works the same
    way as Fizz. So you know you can apply the same logic here for Buzz i.e.
    (i % 5 == 0).
    
    Run test and watch it green.

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -2,7 +2,7 @@
 public class FizzBuzz {
 
 	public String say(int i) {
-		if (i == 5) return "Buzz";
+		if (i % 5 == 0) return "Buzz";
 		if (i % 3 == 0) return "Fizz";
 		return String.valueOf(i);
 	}
