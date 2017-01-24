3bec2c7f0f90d86eea702ec2f937a30f192f91eb
pacroy <pacroy@gmail.com>
2017-01-24 22:21:53 +0700

    Solved the test case with input=15, output=FizzBuzz
    Solve the problem with the same way we did for Fizz (3) and Buzz (5)
    i.e. add a new "if" condition at the top. Green bar now.

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -2,6 +2,7 @@
 public class FizzBuzz {
 
 	public String say(int i) {
+		if (i == 15) return "FizzBuzz";
 		if (i % 5 == 0) return "Buzz";
 		if (i % 3 == 0) return "Fizz";
 		return String.valueOf(i);
