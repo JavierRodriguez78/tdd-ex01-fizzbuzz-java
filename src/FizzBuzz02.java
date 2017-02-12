562319f54e604a26f6c8c031b98146daf23816b7
pacroy <pacroy@gmail.com>
2017-02-12 09:25:31 +0700

    Create FizzBuzz02 and FizzBuzz02Test for exercise 1.2
    Duplicate FizzBuzz into FizzBuzz02 and
    FizzBuzzTest into FizzBuzz02Test
    
    We will leave FizzBuzz untouched there for reference to exercise 1.1 and
    will work on FizzBuzz02 for exercise 1.2

---
+++ src/FizzBuzz02.java
@@ -0,0 +1,23 @@
+
+public class FizzBuzz02 {
+
+	public String say(int i) {
+		String result = "";
+		if (isMultipleOfThree(i)) result += "Fizz";
+		if (isMultipleofFive(i)) result += "Buzz";
+		if (result.isEmpty()) {
+			return String.valueOf(i);
+		} else {
+			return result; 
+		}
+	}
+
+	private boolean isMultipleofFive(int i) {
+		return i % 5 == 0;
+	}
+
+	private boolean isMultipleOfThree(int i) {
+		return i % 3 == 0;
+	}
+	
+}
---
+++ src/FizzBuzz02Test.java
@@ -0,0 +1,40 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FizzBuzz02Test {
+
+	@Test
+	public void should_return_number_when_not_multiple_of_any() {
+		FizzBuzz02 fb = getNewFizzBuzz();
+		assertEquals("1", fb.say(1));
+		assertEquals("2", fb.say(2));
+	}
+
+	@Test
+	public void should_return_fizz_when_multiples_of_three() {
+		FizzBuzz02 fb = getNewFizzBuzz();
+		assertEquals("Fizz", fb.say(3));
+		assertEquals("Fizz", fb.say(6));
+		assertEquals("Fizz", fb.say(9));
+	}
+	
+	@Test
+	public void should_return_buzz_when_multiples_of_five() {
+		FizzBuzz02 fb = getNewFizzBuzz();
+		assertEquals("Buzz", fb.say(5));
+		assertEquals("Buzz", fb.say(10));
+	}
+	
+	@Test
+	public void should_return_fizzbuzz_when_multiples_of_fifteen() {
+		FizzBuzz02 fb = getNewFizzBuzz();
+		assertEquals("FizzBuzz", fb.say(15));
+		assertEquals("FizzBuzz", fb.say(30));
+	}
+
+	private FizzBuzz02 getNewFizzBuzz() {
+		return new FizzBuzz02();
+	}
+
+}
