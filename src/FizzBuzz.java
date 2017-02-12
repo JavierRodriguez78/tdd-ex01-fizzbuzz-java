b8de59b8a45d793a292488496e4d101fef81da0f
pacroy <pacroy@gmail.com>
2017-02-12 08:58:26 +0700

    Extract duplicated logics into methods
    We extracted duplicated logics into new methods  i.e. 
    isMultipleOfThree
    isMultipleofFive
    
    However, we still do have duplicated codes in method say. Do you
    something in common?
    isMultipleOfThree always comes with "Fizz"
    isMultipleofFive always come with "Buzz"
    
    What if we change the logic to concat the output string instead?

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -2,10 +2,18 @@
 public class FizzBuzz {
 
 	public String say(int i) {
-		if ((i % 3 == 0) && (i % 5 == 0)) return "Fizz" + "Buzz";
-		if (i % 5 == 0) return "Buzz";
-		if (i % 3 == 0) return "Fizz";
+		if (isMultipleOfThree(i) && isMultipleofFive(i)) return "Fizz" + "Buzz";
+		if (isMultipleofFive(i)) return "Buzz";
+		if (isMultipleOfThree(i)) return "Fizz";
 		return String.valueOf(i);
+	}
+
+	private boolean isMultipleofFive(int i) {
+		return i % 5 == 0;
+	}
+
+	private boolean isMultipleOfThree(int i) {
+		return i % 3 == 0;
 	}
 
 }
